2263418021d5d9446b56df3f6d7e2ac5b1446f50
Gustavo Henrique Martins <guthhm@gmail.com>
2019-10-07 19:37:17 +0100

    Adicionada impressao da matriz na tela

--- distribuicao.c
+++ distribuicao.c
@@ -2,5 +2,18 @@
 #include <stdlib.h>
 
 int main (void) {
+
+    char matriz_dist[8][8];
+
+    printf("   1  2  3  4  5  6  7  8\n");
+    for (int i = 0; i < 8; i++) {
+        printf("%d ", i+1);
+        for (int j = 0; j < 8; j++) {
+            matriz_dist[i][j] = '*';
+            printf(" %c ", matriz_dist[i][j]);
+        }    
+        printf("\n");    
+    }
+    
     return 0;
 }
