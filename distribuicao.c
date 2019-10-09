9cef5ecf6c6b80e1d0bf47a278e368ce16998890
Gustavo Henrique Martins <guthhm@gmail.com>
2019-10-09 09:51:14 +0100

    Adicinado comentário incial, * substituido por .

--- distribuicao.c
+++ distribuicao.c
@@ -1,3 +1,16 @@
+/*=====================================================================================================================
+Gerenciador de distribuição (@version) v0.01;
+Created by (@author): Gustavo Martins e Braulindo Moreira;
+Número IST: 91632;
+Curso: Mestrado Integrado em Engenharia Eletrotécnica e de Computadores (MEEC);
+Disciplina: Programação;
+Ano: 2019;
+
+Descrição: Este programa consiste em organizar de acordo com regras predefinidas a distribuição dos funcionários e aeronaves
+de uma empresa fictícia e exibir esta disbribuição de forma gráfica no terminal;
+
+======================================================================================================================*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,7 +22,7 @@ int main (void) {
     for (int i = 0; i < 8; i++) {
         printf("%d ", i+1);
         for (int j = 0; j < 8; j++) {
-            matriz_dist[i][j] = '*';
+            matriz_dist[i][j] = '.';
             printf(" %c ", matriz_dist[i][j]);
         }    
         printf("\n");    
